b07549ef7ebf23fdb076a2cd6da64658dd4b7e9b
Lucasdps45 <luckinhasdps45@gmail.com>
2026-08-03 15:45:41 -0300

    Initial Setup: models, database and templates

---
+++ database.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
+
+load_dotenv()
+
+db_url = os.environ.get('DATABASE_URL')
+engine = create_engine(db_url)
+
+SessionLocal = sessionmaker(bind=engine,autoflush=False, autocommit=False)
+
+class Base(DeclarativeBase):
+    pass
+
---
+++ models.py
@@ -0,0 +1,13 @@
+from database import Base
+from typing import Optional
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Text
+
+class Gift(Base):
+    __tablename__ = 'presentes'
+
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    nome: Mapped[str] = mapped_column(String(100))
+    link: Mapped[str] = mapped_column(Text)
+    reservado_por: Mapped[Optional[str]] = mapped_column(String(50))
